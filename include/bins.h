c802aee866aae3731c7dfc09b5a13b3e7d3604e8
Zeref <zeref18.drgnl@gmail.com>
2026-06-04 18:46:31 +0530

    fmt

--- include/bins.h
+++ include/bins.h
@@ -2,16 +2,16 @@
 
 #define DEFAULT_MAX_FASTBIN_SIZE 64
 
-#define NBINS              96 // total bins
-#define NSMALLBINS         32 // small bins seperated by 8 bytes each(8..=248)
+#define NBINS 96      // total bins
+#define NSMALLBINS 32 // small bins seperated by 8 bytes each(8..=248)
 #define NFASTBINS (fastbin_index(request_2_size(MAX_FAST_SIZE)) + 1)
 
-#define SMALLBIN_WIDTH      8
-#define MIN_LARGE_SIZE    256 // the size of the starting large bin
+#define SMALLBIN_WIDTH 8
+#define MIN_LARGE_SIZE 256 // the size of the starting large bin
 
-#define in_smallbin_range(sz)  \
+#define in_smallbin_range(sz)                                                  \
   ((CHUNK_SIZE_T)(sz) < (CHUNK_SIZE_T)MIN_LARGE_SIZE)
 
-#define smallbin_index(sz)     (((unsigned)(sz)) >> 3)
-#define fastbin_index(sz)      (((sz) >> 3) - 2)
-#define is_bin_empty(bin)      ((bin) == bin->data && bin == bin->next_chunk)
+#define smallbin_index(sz) (((unsigned)(sz)) >> 3)
+#define fastbin_index(sz) (((sz) >> 3) - 2)
+#define is_bin_empty(bin) ((bin) == bin->data && bin == bin->next_chunk)
--- include/common.h
+++ include/common.h
@@ -8,11 +8,11 @@
 #define MALLOC_ALIGN_MASK (MALLOC_ALIGNMENT - 1)
 #define CHUNK_SIZE_T unsigned long
 #define MIN_CHUNK_SIZE (sizeof(struct chunk_header))
-#define MIN_REQ_SIZE                                \
+#define MIN_REQ_SIZE                                                           \
   (CHUNK_SIZE_T)(((MIN_CHUNK_SIZE + MALLOC_ALIGN_MASK) & ~MALLOC_ALIGN_MASK))
 #define SYS_ALLOC_PAGE_SIZE 4096
 
-#define request_2_size(req) \
-    ((req) + SIZE_SZ + MALLOC_ALIGN_MASK > MIN_REQ_SIZE) ?  \
-    MIN_REQ_SIZE :                                          \
-    ((req) + SIZE_SZ + MALLOC_ALIGN_MASK) & ~MALLOC_ALIGN_MASK
+#define request_2_size(req)                                                    \
+  ((req) + SIZE_SZ + MALLOC_ALIGN_MASK > MIN_REQ_SIZE)                         \
+      ? MIN_REQ_SIZE                                                           \
+      : ((req) + SIZE_SZ + MALLOC_ALIGN_MASK) & ~MALLOC_ALIGN_MASK
